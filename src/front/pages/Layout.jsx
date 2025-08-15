54107e9b4f6041c174b80e76496cf14c736ec8cf
Sebastian <Sebasleal.esp@gmail.com>
2025-08-15 03:14:55 +0200

    Refactorice el componente Footer y actualice la estructura de diseño para mejorar la legibilidad y la organización

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -1,15 +1,21 @@
-import { Outlet } from "react-router-dom/dist"
-import ScrollToTop from "../components/ScrollToTop"
-import { Navbar } from "../components/Navbar"
-import { Footer } from "../components/Footer"
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
+// Base layout: navbar arriba, contenido que empuja, footer abajo
 export const Layout = () => {
     return (
-        <ScrollToTop>
-            <Navbar />
-                <Outlet />
-            <Footer />
-        </ScrollToTop>
-    )
-}
+        <div className="d-flex flex-column min-vh-100">
+            <ScrollToTop>
+                <Navbar />
+
+                <main className="flex-grow-1">
+                    <Outlet />
+                </main>
+
+                <Footer />
+            </ScrollToTop>
+        </div>
+    );
+};
