3ac56516186d0fa0f5d9f34627de9d0638f421f9
Jose <jm.bebor@gmail.com>
2025-08-19 19:41:55 +0000

    [FEAT]Profile page added with UserCard and Card

--- src/front/components/UserCard.jsx
+++ src/front/components/UserCard.jsx
@@ -10,17 +10,22 @@ export const UserCard = (props) => {
 
     return (
         
-            <div className="card mb-3">
-                <div className="row g-0">
-                    <div className="col-md-4">
-                        <img src="https://placehold.co/600x400" className="img-fluid rounded-circle" alt="..." />
+            <div className="card m-3">
+                <div className="container-fluid d-flex row g-0">
+                    <div className="d-flex col-4 justify-content-center p-1">
+                        <img src="https://placehold.co/600x400" className="img-fluid rounded-circle " alt="..." />
                     </div>
-                    <div className="col-md-8">
+                    <div className="col-4">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                            <p className="card-title">{props.username}Username <i class="fa-solid fa-pen"></i></p>
+                            <p>{props.email}Email <i class="fa-solid fa-pen"></i></p>
+                            <p className="card-text"><small className="text-body-secondary">{props.contact}Contact</small> <i class="fa-solid fa-pen"></i></p>
                         </div>
+                    </div>
+                    <div className="col m-3">
+                        <button className="btn btn-success">
+                            Post a place
+                        </button>
                     </div>
                 </div>
             </div>
--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -2,9 +2,10 @@ import { Link, useParams } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 import React, { useState, useEffect } from "react";
 
-import { UserCard } from "../components/UserCard";
+import { UserCard} from "../components/UserCard";
+import { Card } from "../components/Card";
 
-export const Profile = ()=>{
+export const Profile = () => {
 
 
 
@@ -13,9 +14,91 @@ export const Profile = ()=>{
 
     return (
         <>
-        
-        <UserCard/>
-        
+
+            <UserCard />
+
+<div className="accordion-item">
+    <h2 className="accordion-header m-3">
+      <button className="accordion-button accordion" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Your saved/favorite spaces
+      </button>
+    </h2>
+    <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <div className="d-flex flex-row overflow-auto gap-3 p-3">
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="accordion-item">
+    <h2 className="accordion-header m-3">
+      <button className="accordion-button accordion" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne">
+        Your bookings
+      </button>
+    </h2>
+    <div id="collapseTwo" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <div className="d-flex flex-row overflow-auto gap-3 p-3">
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="accordion-item">
+    <h2 className="accordion-header m-3">
+      <button className="accordion-button accordion" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseOne">
+        Your booked spaces
+      </button>
+    </h2>
+    <div id="collapseThree" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <div className="d-flex flex-row overflow-auto gap-3 p-3">
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        </div>
+      </div>
+    </div>
+  </div>
+  <div className="accordion-item">
+    <h2 className="accordion-header m-3">
+      <button className="accordion-button accordion" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseOne">
+        Your available spaces
+      </button>
+    </h2>
+    <div id="collapseFour" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div className="accordion-body">
+        <div className="d-flex flex-row overflow-auto gap-3 p-3">
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                        </div>
+      </div>
+    </div>
+  </div>
+ 
+
+            
+
         </>
     )
 }
