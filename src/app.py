2c68c7f529a87c9156981337bc9e1c12eac0c37a
Namestruky <fmangom292@gmail.com>
2025-09-08 11:37:59 +0200

    [Fix] Move Flask app initialization to the correct position in the code

--- src/app.py
+++ src/app.py
@@ -17,12 +17,12 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 
 
+app = Flask(__name__)
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../dist/')
 
-app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
